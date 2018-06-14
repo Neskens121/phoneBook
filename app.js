7a27061ea44f2da39995fa5d5a98a40bf10e0fd0
Nenad Bojkovic <nenad.bojkovic@uapdc.local>
2018-06-14 10:43:48 +0200

    added password to mysql root user

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express'),
     var connection = mysql.createConnection({
     	host     : 'localhost',
     	user     : 'root',
-    	password : '',
+    	password : '123456',
     	database : 'phonebook'
     });
 
