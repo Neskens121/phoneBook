8373deb76c32482f20ced2ccd3fc099a5f634ca8
Neskens121 <nenadb92@yahoo.com>
2018-06-17 09:45:24 +0200

    phonenumber validation added

--- public/js/script.js
+++ public/js/script.js
@@ -6,13 +6,11 @@
         regexp = {
           provider: /[a-zA-Z0-9\.\-\_]+/i,
           email: /\@([a-zA-z0-9\-\_\.]+)/i,
-          phoneNumber: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i
+          phoneNumber: /^(\+381|0)?(\s|-)?6(([0-6]|[8-9])\d{6,8}|(77|78)\d{7}){1}$/ig
         },
         elements = list.children().not(exclude),
         filter = function() {
-          var term = input.val().toLowerCase().trim();
-          
-            
+          var term = input.val().toLowerCase().trim();          
           elements.show().filter(function() {
             var text = $(this).text().toLowerCase().trim(),
                 open = term.replace(regexp.provider, '').trim(),
@@ -52,3 +50,30 @@
   $('#search').liveSearch($('.list'), ':last-child');
 
 })(jQuery);
+
+$( document ).ready(function() {
+    $('#contact-form').on('submit', function(){
+      var phoneNumber = /^(\+381|0)?(\s|-)?6(([0-6]|[8-9])\d{6,8}|(77|78)\d{7}){1}$/ig;
+      if ( phoneNumber.test($('#phonenumber').val())) {
+        return;
+      }
+      $('label[for="phonenumber"]').after("<span style='color:red;'> - Invalid Number</span>");
+      event.preventDefault();
+    });
+});
+
+
+$( document ).ready(function() {
+    $('#phonenumber').on('keyup change', function(){
+      var phoneNumber = /^(\+381|0)?(\s|-)?6(([0-6]|[8-9])\d{6,8}|(77|78)\d{7}){1}$/ig;
+      if ($(this).val().length > 3) {
+        if (phoneNumber.test($(this).val())) {
+          $(this).css('color', 'green');
+        } else {
+          $(this).css('color', 'red');
+        }
+      } else {
+        $(this).css('color', 'black'); 
+      }
+    });
+});
